e98f9be12ac32bc84d8e9f6e8bf509689dd15993
Jancy Daniel <jancyjaya12@gmail.com>
2024-07-15 13:36:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const counter = document.querySelector(".counter")
+const barEl = document.querySelector(".loading-bar-front")
+
+let idx = 0;
+
+updateNum()
+
+function updateNum(){
+  counter.innerHTML = "🏃‍♀️‍➡️ " + idx+ "%"
+  barEl.style.width = idx + "%" 
+  idx++
+  if(idx<101){
+    setTimeout(updateNum,20);
+  }
+}
