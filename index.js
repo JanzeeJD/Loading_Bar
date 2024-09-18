cf1595aabe42f8c9afeec65ea6c1cf525462a99d
Jancy Daniel <jancyjaya12@gmail.com>
2024-09-18 11:30:00 +0530

    style: format spacing in counter display

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let idx = 0;
 updateNum()
 
 function updateNum(){
-  counter.innerHTML = "🏃‍♂️ " + idx+ "%"
+  counter.innerHTML = "🏃‍♂️  " + idx + " %";
   barEl.style.width = idx + "%" 
   idx++
   console.log("Loading started...");
